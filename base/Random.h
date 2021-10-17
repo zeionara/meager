975b5970a9e14ad05c9e880271dc1e24e142190e
Dmitrii <zeionara@niuitmo.ru>
2021-10-17 22:42:05 +0300

    Added a more extensive description for the randomization module

--- base/Random.h
+++ base/Random.h
@@ -6,13 +6,13 @@
 unsigned long long *next_random;
 
 extern "C"
-void randReset() {
+void randReset() { // Generate random numbers for each thread
 	next_random = (unsigned long long *)calloc(workThreads, sizeof(unsigned long long));
 	for (INT i = 0; i < workThreads; i++)
 		next_random[i] = rand();
 }
 
-unsigned long long randd(INT id) {
+unsigned long long randd(INT id) { // Generate sequential random numbers
 	next_random[id] = next_random[id] * (unsigned long long)25214903917 + 11;
 	return next_random[id];
 }
@@ -24,8 +24,7 @@ INT rand_max(INT id, INT x) {
 	return res;
 }
 
-//[a,b)
-INT rand(INT a, INT b){
+INT rand(INT a, INT b){ // Sample a random integer in the interval [a, b)
 	return (rand() % (b-a))+ a;
 }
 #endif
