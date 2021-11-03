7e4020128981751847e228e3b61bc71a1e0fc49f
Dmitrii <zeionara@gmail.com>
2021-11-03 14:01:18 +0300

    Updated methods for running validation to match the functionality implemented in the functions for testing

--- base/Test.h
+++ base/Test.h
@@ -31,8 +31,17 @@ extern "C"
 void getHeadBatch(INT *ph, INT *pt, INT *pr) { // Generate all possible triples for every entity which may be used as a triple head
     for (INT i = 0; i < entityTotal; i++) {
         ph[i] = i;
-        pt[i] = testList[lastHead].t;
-        pr[i] = testList[lastHead].r;
+        pt[i] = validList[lastHead].t;
+        pr[i] = validList[lastHead].r;
+    }
+}
+
+extern "C"
+void getValiddHeadBatch(INT *ph, INT *pt, INT *pr) { // Generate all possible triples for every entity which may be used as a triple head
+    for (INT i = 0; i < entityTotal; i++) {
+        ph[i] = i;
+        pt[i] = validList[lastHead].t;
+        pr[i] = validList[lastHead].r;
     }
 }
 
@@ -42,6 +51,15 @@ void getTailBatch(INT *ph, INT *pt, INT *pr) {
         ph[i] = testList[lastTail].h;
         pt[i] = i;
         pr[i] = testList[lastTail].r;
+    }
+}
+
+extern "C"
+void getValidTailBatch(INT *ph, INT *pt, INT *pr) {
+    for (INT i = 0; i < entityTotal; i++) {
+        ph[i] = validList[lastTail].h;
+        pt[i] = i;
+        pr[i] = validList[lastTail].r;
     }
 }
 
@@ -112,10 +130,135 @@ void testHead(REAL *probabilities) {
 }
 
 extern "C"
+void validHead(REAL *probabilities) {
+    INT reference_head = validList[lastHead].h;
+    INT reference_tail = validList[lastHead].t;
+    INT reference_rel = validList[lastHead].r;
+    INT lef = head_lef[reference_rel], rig = head_rig[reference_rel]; // Left and right bound of sets of entity-ids in the common list of acceptable entities for relationships (see type_constraints)
+    // printf("lef = %ld, rig = %ld, rel = %ld", lef, rig, reference_rel);
+
+    REAL reference_distance = probabilities[reference_head];
+    INT l_s = 0;
+    INT l_filter_s = 0;
+    INT l_s_constrain = 0;
+    INT l_filter_s_constrain = 0;
+
+    for (INT hypothesis_head = 0; hypothesis_head < entityTotal; hypothesis_head++) {
+        if (hypothesis_head != reference_head) { 
+            REAL hypothesis_distance = probabilities[hypothesis_head];
+            if (hypothesis_distance < reference_distance) { // If model thinks that the reference triple is more probable (for transe that means less distance between h + r and t)
+                l_s += 1; // Count incorrectly classified triples
+                if (not _find(hypothesis_head, reference_tail, reference_rel)) // If less probable triple is not present in the dataset
+                    l_filter_s += 1; // Count incorrectly classified triples which are not present in the dataset (filtered score must be better than unfiltered concerning rank)
+            }
+
+            // printf("Initial head type at %ld = %ld", lef, head_type[lef]);
+            while (lef < rig && head_type[lef] < hypothesis_head) lef ++;
+            if (lef < rig && hypothesis_head == head_type[lef]) {
+                if (hypothesis_distance < reference_distance) {
+                    l_s_constrain += 1; // Count incorrectly classified triples the head of which is presented in type constraint list for heads (constrained score must be better than unconstrained)
+                    if (not _find(hypothesis_head, reference_tail, reference_rel)) {
+                        l_filter_s_constrain += 1; // Count incorrectly classified triples the head of which is presented in type constraint list for heads but triple does not exist
+                    }
+                }  
+            }
+            // else {
+            //     printf("No head %ld \n", hypothesis_head);
+            // }
+        }
+    }
+
+    if (l_filter_s < 10) l_filter_tot += 1;
+    if (l_s < 10) l_tot += 1;
+    if (l_filter_s < 3) l3_filter_tot += 1;
+    if (l_s < 3) l3_tot += 1;
+    if (l_filter_s < 1) l1_filter_tot += 1;
+    if (l_s < 1) l1_tot += 1;
+
+    if (l_filter_s_constrain < 10) l_filter_tot_constrain += 1;
+    if (l_s_constrain < 10) l_tot_constrain += 1;
+    if (l_filter_s_constrain < 3) l3_filter_tot_constrain += 1;
+    if (l_s_constrain < 3) l3_tot_constrain += 1;
+    if (l_filter_s_constrain < 1) l1_filter_tot_constrain += 1;
+    if (l_s_constrain < 1) l1_tot_constrain += 1;
+
+    l_filter_rank += (l_filter_s+1);
+    l_rank += (1+l_s);
+    l_filter_reci_rank += 1.0/(l_filter_s+1);
+    l_reci_rank += 1.0/(l_s+1);
+
+    l_filter_rank_constrain += (l_filter_s_constrain+1);
+    l_rank_constrain += (1+l_s_constrain);
+    l_filter_reci_rank_constrain += 1.0/(l_filter_s_constrain+1);
+    l_reci_rank_constrain += 1.0/(l_s_constrain+1);
+
+    lastHead++;
+}
+
+extern "C"
 void testTail(REAL *probabilities) {
     INT h = testList[lastTail].h;
     INT t = testList[lastTail].t;
     INT r = testList[lastTail].r;
+    INT lef = tail_lef[r], rig = tail_rig[r];
+    REAL reference_distance = probabilities[t];
+    INT r_s = 0;
+    INT r_filter_s = 0;
+    INT r_s_constrain = 0;
+    INT r_filter_s_constrain = 0;
+    for (INT j = 0; j < entityTotal; j++) {
+        if (j != t) {
+            REAL hypothesis_distance = probabilities[j];
+            if (hypothesis_distance < reference_distance) {
+                r_s += 1;
+                if (not _find(h, j, r))
+                    r_filter_s += 1;
+            }
+            while (lef < rig && tail_type[lef] < j) lef ++;
+            if (lef < rig && j == tail_type[lef]) {
+                    if (hypothesis_distance < reference_distance) {
+                        r_s_constrain += 1;
+                        if (not _find(h, j ,r)) {
+                            r_filter_s_constrain += 1;
+                        }
+                    }
+            }
+        }
+        
+    }
+
+    if (r_filter_s < 10) r_filter_tot += 1;
+    if (r_s < 10) r_tot += 1;
+    if (r_filter_s < 3) r3_filter_tot += 1;
+    if (r_s < 3) r3_tot += 1;
+    if (r_filter_s < 1) r1_filter_tot += 1;
+    if (r_s < 1) r1_tot += 1;
+
+    if (r_filter_s_constrain < 10) r_filter_tot_constrain += 1;
+    if (r_s_constrain < 10) r_tot_constrain += 1;
+    if (r_filter_s_constrain < 3) r3_filter_tot_constrain += 1;
+    if (r_s_constrain < 3) r3_tot_constrain += 1;
+    if (r_filter_s_constrain < 1) r1_filter_tot_constrain += 1;
+    if (r_s_constrain < 1) r1_tot_constrain += 1;
+
+    r_filter_rank += (1+r_filter_s);
+    r_rank += (1+r_s);
+    r_filter_reci_rank += 1.0/(1+r_filter_s);
+    r_reci_rank += 1.0/(1+r_s);
+
+    r_filter_rank_constrain += (1+r_filter_s_constrain);
+    r_rank_constrain += (1+r_s_constrain);
+    r_filter_reci_rank_constrain += 1.0/(1+r_filter_s_constrain);
+    r_reci_rank_constrain += 1.0/(1+r_s_constrain);
+
+    lastTail++;
+}
+
+extern "C"
+void validTail(REAL *probabilities) {
+    INT h = validList[lastTail].h;
+    INT t = validList[lastTail].t;
+    INT r = validList[lastTail].r;
     INT lef = tail_lef[r], rig = tail_rig[r];
     REAL reference_distance = probabilities[t];
     INT r_s = 0;
