5cc6f8444a837d3f0fb6f338d285a58c88f7bda1
zeio <zeionara@gmail.com>
2022-11-28 23:53:44 +0300

    Connected to the randeer library for generating random numbers

---
+++ .vimrc
@@ -0,0 +1,4 @@
+" let g:ale_cpp_cc_options = '-std=c++20 -I ' . $HOME . '/randeer/randeer'
+
+let g:ale_cpp_cc_options = '-std=c++20 -I $HOME/randeer/randeer'
+let g:ale_cpp_clangd_options = '-std=c++20 -I$HOME/randeer/randeer'
--- base/Random.cpp
+++ base/Random.cpp
@@ -1,18 +1,59 @@
-#include "types.h"
 #include <cstdlib>
 
-unsigned long long *next_random;
+#include "types.h"
+// #include "../../randeer/randeer/state/RandomizationState.h"
+#include "state/RandomizationState.h"
+#include "state/JavaLikeLcgRandomizationState.h"
+
+using namespace std;
+
+RandomizationState** randomizationStates;
+// unsigned long long* next_random;
+
+long k = 0;
 
 extern "C"
 void randReset(INT nWorkers) { // Generate random numbers for each thread
-	next_random = (unsigned long long *)calloc(nWorkers, sizeof(unsigned long long));
-	for (INT i = 0; i < nWorkers; i++)
-		next_random[i] = rand();
+	// next_random = (unsigned long long *)calloc(nWorkers, sizeof(unsigned long long));
+	randomizationStates = (RandomizationState**)calloc(nWorkers, sizeof(RandomizationState*));
+	for (INT i = 0; i < nWorkers; i++) {
+		// next_random[i] = rand();
+        //
+        // long seed = rand();
+        // cout << "Init randomizer" << endl;
+		// randomizationStates[i] = new JavaLikeLcgRandomizationState(seed);
+		randomizationStates[i] = new JavaLikeLcgRandomizationState(rand());
+        // cout << randomizationStates[i]->state << endl;
+        // randomizationStates[i]->reset(randa + 1);
+        // cout << randomizationStates[i]->multiplier * randomizationStates[i]->state << endl;
+        // cout << randomizationStates[i]->sample() << endl;
+		// next_random[i] = seed;
+
+        // cout << "vv" << endl;
+        // cout << randomizationStates[i]->state << endl;
+        // cout << next_random[i] << endl;
+        // cout << "^^" << endl;
+    }
 }
 
 unsigned long long randd(INT id) { // Generate sequential random numbers
-	next_random[id] = next_random[id] * (unsigned long long)25214903917 + 11;
-	return next_random[id];
+    // next_random[id] = next_random[id] * (unsigned long long)25214903917 + 11;
+    // return next_random[id];
+    //
+    // unsigned long long value = next_random[id];
+	// long value = randomizationStates[id]->sample();
+    // std::cout << value << std::endl;
+    // unsigned long long sampledValue = randomizationStates[id]->sample();
+    // if (k < 5) {
+    //     cout << "--" << endl;
+    //     cout << sampledValue << endl;
+    //     cout << value << endl;
+    //     cout << "==" << endl;
+    // }
+    // k += 1;
+    // return value;
+    // return sampledValue;
+    return randomizationStates[id]->sample();
 }
 
 INT rand_max(INT id, INT x) {
