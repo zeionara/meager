b5f0008b94782d413a72bd0340f76537cd839692
zeio <zeionara@gmail.com>
2022-11-29 20:54:04 +0300

    Refactored none pattern description

--- base/patterns/main.h
+++ base/patterns/main.h
@@ -47,6 +47,38 @@ struct PatternDescription {
     int nTriplesPerInstance;
     vector<PatternInstance>** instanceSets;
 
+    void initInstanceLists() {
+        instanceSets = (vector<PatternInstance>**)malloc(sizeof(vector<PatternInstance>*) * (nTriplesPerInstance + 1));
+
+        for (int i = 0; i <= nTriplesPerInstance; i++) {
+            instanceSets[i] = new vector<PatternInstance>;
+        }
+    }
+
+    void push(PatternInstance patternInstance) {
+        for (int j = 0; j <= nTriplesPerInstance; j++) {
+            if (j <= patternInstance.observedTripleIndices.size()) {
+               instanceSets[j]->push_back(patternInstance); 
+            }
+        }
+    }
+
+    void tryPush(PatternInstance patternInstance, unordered_set<string> seenInstances, bool dropDuplicates = true) {
+        if (dropDuplicates) {
+            string direct_pattern_instance_concise_description = patternInstance.getConciseDescription();
+
+            if (seenInstances.find(direct_pattern_instance_concise_description) == seenInstances.end()) {
+                push(patternInstance);
+                seenInstances.insert(direct_pattern_instance_concise_description);
+            }
+            // } else {
+            //     cout << direct_pattern_instance_concise_description << endl;
+            // }
+        } else {
+            push(patternInstance);
+        }
+    }
+
     // PatternDescription(Pattern id_ = none, int nTriplesPerInstance_ = 10, vector<PatternInstance>** instanceSets_ = 0) {
     //     id = id_;
     //     nTriplesPerInstance = nTriplesPerInstance_; 
--- base/patterns/none/NonePatternDescription.h
+++ base/patterns/none/NonePatternDescription.h
@@ -7,77 +7,33 @@
 #include "../../triple/list/TripleList.h"
 
 struct NonePatternDescription: PatternDescription {
-    // Pattern id;
-    // int nTriplesPerInstance;
-    // vector<PatternInstance>** instanceSets;
 
     NonePatternDescription(TripleList* triples, bool verbose = false, bool dropDuplicates = true) {
-        INT nTriplesPerInstance_ = 1;
-        // cout << sizeof(vector<PatternInstance>*) * (nTriplesPerInstance_ + 1) << endl;
-        vector<PatternInstance>** instances = (vector<PatternInstance>**)malloc(sizeof(vector<PatternInstance>*) * (nTriplesPerInstance_ + 1));
+
+        id = none;
+        nTriplesPerInstance = 1;
+
+        initInstanceLists();
 
         unordered_set<string> seenInstances;
 
-        for (int i = 0; i <= nTriplesPerInstance_; i++) {
-            instances[i] = new vector<PatternInstance>;
-        }
-
-        // cout << "none triples handling checkpoint #1" << endl;
-
         for (INT i = 0; i < triples->length; i++) { // Reading train samples
-            // cout << "none triples handling checkpoint #2" << endl;
-
-            NonePatternInstance patternInstance = NonePatternInstance(
-                triples->items[i]
+            tryPush(
+                NonePatternInstance(triples->items[i]),
+                seenInstances, dropDuplicates
             );
-
-            // cout << "none triples handling checkpoint #3" << endl;
-
-            if (dropDuplicates) {
-                string direct_pattern_instance_concise_description = patternInstance.getConciseDescription();
-                if (seenInstances.find(direct_pattern_instance_concise_description) == seenInstances.end()) {
-                    for (int j = 0; j <= nTriplesPerInstance_; j++) {
-                        if (j <= patternInstance.observedTripleIndices.size()) {
-                           instances[j]->push_back(patternInstance); 
-                        }
-                    }
-                    seenInstances.insert(direct_pattern_instance_concise_description);
-                }
-                // } else {
-                //     cout << direct_pattern_instance_concise_description << endl;
-                // }
-            } else {
-                for (int j = 0; j <= nTriplesPerInstance_; j++) {
-                    if (j <= patternInstance.observedTripleIndices.size()) {
-                       instances[j]->push_back(patternInstance); 
-                    }
-                }
-            }
         }
 
         cout << "Should collect " << triples->length << " none pattern instances" << endl;
 
         if (verbose) {
-            for (int i = 0; i <= nTriplesPerInstance_; i++) {
-                cout << "Collected " << instances[i]->size() << " none pattern instances in which there are " << i << " or more observed patterns" << endl;
+            for (int i = 0; i <= nTriplesPerInstance; i++) {
+                cout << "Collected " << instanceSets[i]->size() << " none pattern instances in which there are " << i << " or more observed patterns" << endl;
             }
-
-            std::cout << std::endl;
-            // std::cout << std::endl << std::endl;
-            
-            // for (PatternInstance patternInstance: **noneTriplePatternInstances) {
-            //     ((NonePatternInstance*)&patternInstance)->print();
-            // }
         }
 
-        // patternDescriptions[nonePatternName].instanceSets = noneTriplePatternInstances;
-
-        id = none;
-        cout << "Setting n triples per instance" << endl;
-        nTriplesPerInstance = 1; 
-        cout << "Set n triples per instance = " << nTriplesPerInstance << endl;
-        instanceSets = instances;
     }
+
 };
 
 #endif
