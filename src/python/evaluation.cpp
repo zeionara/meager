cfa6e61be5c8a9ac1a8a5e860580568f1253f9d0
zeio <zeionara@gmail.com>
2022-12-16 19:10:23 +0300

    Implemented metric tree terminal node encoder

--- src/python/evaluation.cpp
+++ src/python/evaluation.cpp
@@ -33,3 +33,42 @@ extern "C" void meager__python__api__evaluation__evaluate(char* componentName, R
     // cout << endl;
     meager::main::api::evaluation::evaluate(triple::decodeComponent(componentName), predictions, reverse, verbose);
 }
+
+#define METRIC_T double
+
+extern "C" void** meager__python__api__evaluation__computeMetrics(bool verbose) {
+    meager::main::evaluation::metric::tree::Root* root = meager::main::api::evaluation::computeMetrics(verbose);
+
+    // long* foo = new long[1];
+    // foo[0] = 2;
+
+    // long** bar = new long*[1];
+    // bar[0] = foo;
+
+    // // return root;
+    // return (void**)bar;
+
+    const char length = 3;
+
+    unsigned char* result = new unsigned char[sizeof(METRIC_T) * length + sizeof(char*) * length + 1];
+
+    result[0] = length;
+    result[0] |= (unsigned char)0x80;
+
+    METRIC_T* values = (METRIC_T*)(result + 1);
+
+    values[0] = 0.1;
+    values[1] = 0.2;
+    values[2] = 0.3;
+    
+    char** names = (char**)(result + 1 + sizeof(METRIC_T) * length);
+
+    names[0] = (char*)"foo";
+    names[1] = (char*)"bar";
+    names[2] = (char*)"qux";
+
+    unsigned char** bar = new unsigned char*[1];
+    bar[0] = result;
+
+    return (void**)bar;
+}
