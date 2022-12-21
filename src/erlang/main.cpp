503a859bc7ea3a9f1149073e18e98248d0f29637
zeio <zeionara@gmail.com>
2022-12-21 20:57:05 +0300

    Updated function names for erlang adapter

--- src/erlang/main.cpp
+++ src/erlang/main.cpp
@@ -1,5 +1,3 @@
-#include "erl_nif.h"
-
 #include "corpus.h"
 #include "sampling.h"
 #include "evaluation.h"
@@ -9,34 +7,34 @@ namespace meager::erlang::api {
     static ErlNifFunc functions[] = {
         // Corpus
 
-        {"_init_corpus", 4, corpus::init},
+        {"init_corpus", 4, corpus::init},
 
-        {"_import_filter", 2, corpus::importFilter},
-        {"_import_pattern", 1, corpus::importPattern},
-        {"_import_types", 1, corpus::importTypes},
+        {"import_filter", 2, corpus::importFilter},
+        {"import_pattern", 1, corpus::importPattern},
+        {"import_types", 1, corpus::importTypes},
 
-        {"_import_train", 2, corpus::importTrain},
-        {"_import_triples", 2, corpus::importTriples},
+        {"import_train", 2, corpus::importTrain},
+        {"import_triples", 2, corpus::importTriples},
 
-        {"_count_entities", 1, corpus::countEntities},
-        {"_count_relations", 1, corpus::countRelations},
-        {"_count_triples", 1, corpus::countTriples_1},
-        {"_count_triples", 2, corpus::countTriples_2},
+        {"count_entities", 1, corpus::countEntities},
+        {"count_relations", 1, corpus::countRelations},
+        {"count_triples", 1, corpus::countTriples_1},
+        {"count_triples", 2, corpus::countTriples_2},
 
         // Sampler
 
-        {"_init_sampler", 6, sampling::init},
-        {"_sample", 5, sampling::sample},
+        {"init_sampler", 6, sampling::init},
+        {"sample", 5, sampling::sample},
 
         // Evaluator
 
-        {"_init_evaluator", 4, evaluation::init},
-        {"_trial", 2, evaluation::trial},
-        {"_evaluate", 4, evaluation::evaluate},
-        {"_compute_metrics", 1, evaluation::computeMetrics},
+        {"init_evaluator", 4, evaluation::init},
+        {"trial", 2, evaluation::trial},
+        {"evaluate", 4, evaluation::evaluate},
+        {"compute_metrics", 1, evaluation::computeMetrics},
 
     };
 
 }
 
-ERL_NIF_INIT(Elixir.Grapex.Meager, meager::erlang::api::functions, NULL, NULL, NULL, NULL)
+ERL_NIF_INIT(Elixir.Grapex.Meager.Placeholder, meager::erlang::api::functions, NULL, NULL, NULL, NULL)
